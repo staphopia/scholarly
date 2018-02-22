50905778b50c8ef21267874a44c697792c842ba7
Robert A. Petit III <robbie.petit@gmail.com>
2018-02-22 11:21:56 -0500

    Update scholarly.py

--- scholarly.py
+++ scholarly.py
@@ -70,7 +70,7 @@ def _handle_captcha(url):
 def _get_page(pagerequest):
     """Return the data for a page on scholar.google.com"""
     # Note that we include a sleep to avoid overloading the scholar server
-    time.sleep(5+random.uniform(0, 5))
+    time.sleep(2+random.uniform(0, 4))
     resp = _SESSION.get(pagerequest, headers=_HEADERS, cookies=_COOKIES)
     if resp.status_code == 200:
         return resp.text
